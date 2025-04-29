efc6ad93c4ee3a51b7ece49eb593fb0568c3aa10
Theo Davi <150192145+theo-davi@users.noreply.github.com>
2025-04-29 21:31:27 +0000

    att

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, redirect, flash, url_for
-from utils import db
 from flask_migrate import Migrate
+from utils import db
 from models.Mensagem import Mensagem
+from controllers.Mensagem import bp_mensagens
 
 app = Flask(__name__)
+app.register_blueprint(bp_mensagens, url_prefix='/mensagens')
 
-app.config['SECRET_KEY'] = 'teste'
+app.config['SECRET_KEY'] = 'chave_secreta'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///project.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -13,20 +15,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 migrate = Migrate(app, db)
 
-@app.route('/mensagens', methods=['GET', 'POST'])
-def mensagens():
-    if request.method=='GET':
-        return render_template('create.html')
-    elif request.method=='POST':
-        m=(request.form['conteudo'])
-        mensagem=Mensagem(m)
-        db.session.add(mensagem)
-        db.session.commit()
-        return redirect('recovery')
-
-@app.route('/recovery', defaults={'id': 0})
-@app.route('/recovery/<int:id>')
-def recovery(id):
-    if (id==0):
-        mensagens = Mensagem.query.all()
-        return render_template('recovery.html', mensagens=mensagens)
+@app.route('/')
+def index():
+    return render_template('index.html')
