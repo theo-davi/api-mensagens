43fbc4648830b8146d18b4562d416522baab934f
Theo Davi <150192145+theo-davi@users.noreply.github.com>
2025-05-05 11:14:24 +0000

    versão funcional

--- app.py
+++ app.py
@@ -1,58 +1,53 @@
-from flask import Flask, redirect
+from flask import Flask, request, jsonify
 import json
-from flask_migrate import Migrate
-from utils import db
-from models.Mensagem import Mensagem
 
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'chave_secreta'
-
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///project.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db.init_app(app)
-migrate = Migrate(app, db)
-
-arquivo=open('db.json', 'w')
 
 @app.route('/')
 def index():
     return "<a href='/mensagens'>/mensagens</a>"
 
-@app.route('/mensagens')
-def read_all():
-    return json.load(open('db.json'))
-
-@app.route('/mensagens/<id>')
-def read_one(id):
-    for mensagem in dados['mensagens']:
-        if mensagem['id']==id:
-            return mensagem
-        else:
-            return 'Esse ID não existe.'
+mensagens=[]
+auto_id=1
 
 @app.route('/mensagens', methods=['POST'])
 def create():
-    db.session.add(Mensagem('nova mensagem'))
-    db.session.commit()
-    arquivo.write(json.dumps({"id":"mensagem.id", "conteudo":"mensagem.conteudo"}))
-    return redirect('mensagens')
+    global auto_id
+    mensagem={
+        "id":auto_id,
+        "conteudo":request.get_json().get("conteudo")
+    }
+    mensagens.append(mensagem)
+    auto_id+=1
+    arquivo=open('db.json', 'w')
+    arquivo.write(json.dumps({"mensagens":mensagens}))
+    return jsonify({"mensagens":mensagens})
 
-@app.route('/mensagens/<id>', methods=['PUT'])
-def update(id):
-    for mensagem in dados['mensagens']:
+@app.route('/mensagens')
+def read_all():
+    return jsonify({"mensagens":mensagens})
+
+@app.route('/mensagens/<int:id>')
+def read_one(id):
+    for mensagem in mensagens:
         if mensagem['id']==id:
-            mensagem.update({'conteudo':'novo texto da mensagem'})
             return mensagem
-        else:
-            return 'Esse ID não existe.'
+    return 'Mensagem não encontrada.'
 
-@app.route('/mensagens/<id>', methods=['DELETE'])
-def delete(id):
-    for mensagem in dados['mensagens']:
+@app.route('/mensagens/<int:id>', methods=['PUT'])
+def update(id):
+    for mensagem in mensagens:
         if mensagem['id']==id:
-            dados['mensagens'].remove(mensagem)
-            return dados
-        else:
-            return 'Esse ID não existe.'
+            mensagem['conteudo']=request.get_json().get('conteudo')
+            arquivo=open('db.json', 'w')
+            arquivo.write(json.dumps({"mensagens":mensagens}))
+            return mensagem
+
+@app.route('/mensagens/<int:id>', methods=['DELETE'])
+def delete(id):
+    for mensagem in mensagens:
+        if mensagem['id']==id:
+            mensagens.remove(mensagem)
+            arquivo=open('db.json', 'w')
+            arquivo.write(json.dumps({"mensagens":mensagens}))
+            return jsonify({"mensagens":mensagens})
