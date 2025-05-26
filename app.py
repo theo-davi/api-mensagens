c57690eebd7d81b196a4e2248d1db0c6566406e0
Theo Davi <150192145+theo-davi@users.noreply.github.com>
2025-05-26 03:14:54 +0000

    finalizando tratamento de erros

--- app.py
+++ app.py
@@ -28,6 +28,10 @@ class Mensagem(db.Model):
 def conteudo_vazio(error):
     return jsonify({"mensagem":"Mensagem vazia."}), 400
 
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"mensagem":"Mensagem não encontrada."}), 404
+
 @app.route('/')
 def index():
     return "<a href='/mensagens'>/mensagens</a>"
@@ -56,27 +60,24 @@ def create():
 
 @app.route('/mensagens/<int:id>')
 def read_one(id):
-    try:
-        for mensagem in mensagens:
-            if mensagem['id']==id:
-                return mensagem
-    except:
-        return jsonify({"mensagem":"Mensagem não encontrada"}), 404
+    for mensagem in mensagens:
+        if mensagem['id']==id:
+            return mensagem
+    abort(404)
 
 @app.route('/mensagens/<int:id>', methods=['PUT'])
 def update(id):
-    try:
-        for mensagem in mensagens:
-            if mensagem['id']==id:
-                mensagem_bd=Mensagem.query.get(id)
-                mensagem_bd.conteudo=request.get_json().get('conteudo')
-                db.session.add(mensagem_bd)
-                db.session.commit()
-                mensagem['conteudo']=request.get_json().get('conteudo')
-    except:
-        return jsonify({"mensagem":"Mensagem não encontrada"}), 404
-    else:
-        return redirect(url_for('read_one', id=id))
+    for mensagem in mensagens:
+        if mensagem['id']==id:
+            if request.get_json().get('conteudo')=="":
+                abort(400)                    
+            mensagem_bd=Mensagem.query.get(id)
+            mensagem_bd.conteudo=request.get_json().get('conteudo')
+            db.session.add(mensagem_bd)
+            db.session.commit()
+            mensagem['conteudo']=request.get_json().get('conteudo')
+            return redirect(url_for('read_one', id=id))
+    abort(404)
 
 @app.route('/mensagens/<int:id>', methods=['DELETE'])
 def delete(id):
@@ -85,7 +86,7 @@ def delete(id):
         db.session.commit()
         return redirect(url_for('read_all'))
     except:
-        return jsonify({"mensagem":"Mensagem não encontrada"}), 404
+        abort(404)
 
 if __name__ == '__main__':
     app.run()
