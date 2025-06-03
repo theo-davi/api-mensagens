ddfade43812a67633930d6ade51dab104a898604
Theo Davi <150192145+theo-davi@users.noreply.github.com>
2025-06-03 20:58:19 +0000

    modularizacao

--- app.py
+++ app.py
@@ -1,28 +1,19 @@
 from flask import Flask, request, jsonify, redirect, url_for, abort
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from utils import db
+from models.mensagem import Mensagem
+from controllers.mensagem import bp_mensagens
 
 app = Flask(__name__)
+app.register_blueprint(bp_mensagens, url_prefix='/mensagens')
 
 app.config['SECRET_KEY'] = 'teste'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///project.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy()
 db.init_app(app)
 migrate = Migrate(app, db)
-
-class Mensagem(db.Model):
-    __tablename__="mensagem"
-    id = db.Column(db.Integer, primary_key = True)
-    conteudo = db.Column(db.String(280))
-
-    def __init__(self, conteudo):
-        self.conteudo = conteudo
-    
-    def __repr__(self):
-        return "<Mensagem {}>".format(self.conteudo)
 
 @app.errorhandler(400)
 def conteudo_vazio(error):
@@ -35,58 +26,6 @@ def not_found(error):
 @app.route('/')
 def index():
     return "<a href='/mensagens'>/mensagens</a>"
-
-@app.route('/mensagens')
-def read_all():
-    global mensagens
-    mensagens=[]
-    mensagens_bd=Mensagem.query.all()
-    for mensagem_bd in mensagens_bd:
-        mensagens.append({
-            "id":mensagem_bd.id,
-            "conteudo":mensagem_bd.conteudo
-        })
-    return jsonify({"mensagens":mensagens})
-
-@app.route('/mensagens', methods=['POST'])
-def create():
-    mensagem=request.get_json().get("conteudo")
-    if mensagem=="":
-        abort(400)
-    mensagem_bd=Mensagem(mensagem)
-    db.session.add(mensagem_bd)
-    db.session.commit()
-    return redirect(url_for('read_all'))
-
-@app.route('/mensagens/<int:id>')
-def read_one(id):
-    for mensagem in mensagens:
-        if mensagem['id']==id:
-            return mensagem
-    abort(404)
-
-@app.route('/mensagens/<int:id>', methods=['PUT'])
-def update(id):
-    for mensagem in mensagens:
-        if mensagem['id']==id:
-            if request.get_json().get('conteudo')=="":
-                abort(400)                    
-            mensagem_bd=Mensagem.query.get(id)
-            mensagem_bd.conteudo=request.get_json().get('conteudo')
-            db.session.add(mensagem_bd)
-            db.session.commit()
-            mensagem['conteudo']=request.get_json().get('conteudo')
-            return redirect(url_for('read_one', id=id))
-    abort(404)
-
-@app.route('/mensagens/<int:id>', methods=['DELETE'])
-def delete(id):
-    try:
-        db.session.delete(Mensagem.query.get(id))
-        db.session.commit()
-        return redirect(url_for('read_all'))
-    except:
-        abort(404)
 
 if __name__ == '__main__':
     app.run()
---
+++ controllers/mensagem.py
@@ -0,0 +1,57 @@
+from flask import request, jsonify, redirect, url_for, abort, Blueprint
+from models.mensagem import Mensagem
+from utils import db
+
+bp_mensagens = Blueprint("mensagens", __name__, template_folder='templates')
+
+@bp_mensagens.route('/')
+def read_all():
+    global mensagens
+    mensagens=[]
+    mensagens_bd=Mensagem.query.all()
+    for mensagem_bd in mensagens_bd:
+        mensagens.append({
+            "id":mensagem_bd.id,
+            "conteudo":mensagem_bd.conteudo
+        })
+    return jsonify({"mensagens":mensagens})
+
+@bp_mensagens.route('/', methods=['POST'])
+def create():
+    mensagem=request.get_json().get("conteudo")
+    if mensagem=="":
+        abort(400)
+    mensagem_bd=Mensagem(mensagem)
+    db.session.add(mensagem_bd)
+    db.session.commit()
+    return redirect(url_for('read_all'))
+
+@bp_mensagens.route('//<int:id>')
+def read_one(id):
+    for mensagem in mensagens:
+        if mensagem['id']==id:
+            return mensagem
+    abort(404)
+
+@bp_mensagens.route('//<int:id>', methods=['PUT'])
+def update(id):
+    for mensagem in mensagens:
+        if mensagem['id']==id:
+            if request.get_json().get('conteudo')=="":
+                abort(400)                    
+            mensagem_bd=Mensagem.query.get(id)
+            mensagem_bd.conteudo=request.get_json().get('conteudo')
+            db.session.add(mensagem_bd)
+            db.session.commit()
+            mensagem['conteudo']=request.get_json().get('conteudo')
+            return redirect(url_for('read_one', id=id))
+    abort(404)
+
+@bp_mensagens.route('//<int:id>', methods=['DELETE'])
+def delete(id):
+    try:
+        db.session.delete(Mensagem.query.get(id))
+        db.session.commit()
+        return redirect(url_for('read_all'))
+    except:
+        abort(404)
