04c4a2bf57a7dbbbce9b23591505c308dca5c9f0
Theo Davi <150192145+theo-davi@users.noreply.github.com>
2025-06-10 19:16:59 +0000

    remoção de código desnecessário e insercao de comentários

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
-from flask import Flask, request, jsonify, redirect, url_for, abort
+from flask import Flask, jsonify, render_template_string
+'''
+do pacote (framework) flask, são importados:
+- a classe Flask, sua principal, usada para criar uma aplicação web com Python;
+- a função jsonify, usada para converter dicionários Python em respostas no formato JSON;
+- a função render_template_string, usada para renderizar templates HTML a partir de strings.
+'''
 from flask_migrate import Migrate
+#importa, do pacote flask_migrate, a classe Migrate, que gerencia *migrações* (envio de models para se tornarem tabelas) de banco de dados em apps
+#com SQLAlchemy. Com a Migrate, atualiza-se models (que representam tabelas do banco de dados) e versiona-se as alterações.
 from utils import db
-from models.mensagem import Mensagem
+#importa, do módulo utils, a variável db.
 from controllers.mensagem import bp_mensagens
 
 app = Flask(__name__)
+#cria uma instância da classe Flask, ou seja, cria a aplicação web Flask.
 app.register_blueprint(bp_mensagens, url_prefix='/mensagens')
+#registra o blueprint bp_mensagens na aplicação e define um prefixo comum para todas as rotas do bp, começando com /mensagens.
 
 app.config['SECRET_KEY'] = 'teste'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///project.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
+db.init_app(app)#conecta o banco de dados com a aplicação.
 migrate = Migrate(app, db)
 
 @app.errorhandler(400)
@@ -25,7 +35,7 @@ def not_found(error):
 
 @app.route('/')
 def index():
-    return "<a href='/mensagens'>/mensagens</a>"
+    return render_template_string('''<a href="{{url_for('mensagens.read_all')}}">/mensagens</a>''')
 
 if __name__ == '__main__':
     app.run()
--- utils.py
+++ utils.py
@@ -1,2 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
+'''
+importa, do pacote flask_sqlalchemy, a classe SQLAlchemy, que é um ORM, sigla para Object-Relational Mapping, que significa Mapeamento
+Objeto-Relacional, que, no Flask, é responsável por mapear as classes que são modelos para tabelas do banco de dados, identificadas por herdarem
+da classe Model do Flask SQLAlchemy, o ORM do Flask. Assim, mapeia as classes que herdam de Model para se tornarem tabelas do banco de dados. Por
+serem classes, a manipulação de dados pode ser feita de forma orientada a objetos, e não há necessidade de usar SQL diretamente.
+'''
+db = SQLAlchemy()#cria uma instância da classe SQLAlchemy, usada para gerenciar o banco de dados, ou seja, cria o banco de dados.
