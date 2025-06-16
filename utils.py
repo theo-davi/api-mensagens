19f25471204e85cd48cad8768c056bf8873dddf8
Theo Davi <150192145+theo-davi@users.noreply.github.com>
2025-06-16 20:10:16 +0000

    anotacoes

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 importa, do pacote flask_sqlalchemy, a classe SQLAlchemy, que é um ORM, sigla para Object-Relational Mapping, que significa Mapeamento
 Objeto-Relacional, que, no Flask, é responsável por mapear as classes que são modelos para tabelas do banco de dados, identificadas por herdarem
 da classe Model do SQLAlchemy. Assim, mapeia as classes que herdam de Model para se tornarem tabelas do banco de dados. Por
-serem classes, a manipulação de dados pode ser feita de forma orientada a objetos, e não há necessidade de usar SQL diretamente.
+serem classes, a manipulação de dados pode ser feita de forma orientada a objetos (com criação de objetos e uso de métodos - funções), e não há necessidade de
+usar SQL diretamente.
 '''
 db = SQLAlchemy()#cria uma instância da classe SQLAlchemy, usada para gerenciar o banco de dados, ou seja, cria o banco de dados.
